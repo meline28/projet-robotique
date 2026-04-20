93279763ed3c327be5cc637a1d24fbc708109fe8
meline28 <meline.bonny@lycee-pierretermier.org>
2026-04-20 11:41:17 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-	
+    servos.P1.setAngle(90)
 })
-servos.P0.setAngle(90)
+input.onButtonPressed(Button.B, function () {
+    servos.P1.setAngle(45)
+})
+servos.P1.setAngle(155)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
 })
